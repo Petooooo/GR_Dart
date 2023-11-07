e6e4a06057d360eb05884e525129d700980eb985
Peto <peto@khu.ac.kr>
2023-11-07 22:31:57 +0900

    Add 404 Error Handler

--- Server/RESTAPI/restapi.js
+++ Server/RESTAPI/restapi.js
@@ -56,10 +56,12 @@ app.get("/product", (request, response) => {
     );
 });
 
-app.use((request, response) => {
-    response.send(`<h1>Wrong Access</h1>`);
+// 404 Error Handler
+app.use(function (req, res, next) {
+    res.status(404).send("404 Error: Page Not Found");
 });
 
+// Start Server on Port ${portnum}
 app.listen(portnum, () => {
     console.log("Listening on " + portnum);
 });
