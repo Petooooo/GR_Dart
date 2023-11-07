acc6478716a93e5e5c20db84241832f23b113694
Peto <peto@khu.ac.kr>
2023-11-07 19:02:31 +0900

    Modified mysql.sql to add reviewTable

--- Server/Database/mysql.sql
+++ Server/Database/mysql.sql
@@ -18,3 +18,16 @@ CREATE TABLE detailpicUrlTable (
   CONSTRAINT detailpicUrlTable_PK PRIMARY KEY(id),
   CONSTRAINT itemTable_FK FOREIGN KEY(FK_itemTable) REFERENCES itemTable(id)
 );
+
+CREATE TABLE reviewTable (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  check_1 INT(11) NOT NULL,
+  check_2 INT(11) NOT NULL,
+  check_3 INT(11) NOT NULL,
+  check_4 INT(11) NOT NULL,
+  FK_itemTable VARCHAR(11) NOT NULL,
+  CONSTRAINT reviewTable_PK PRIMARY KEY(id),
+  CONSTRAINT itemTable_FK2 FOREIGN KEY(FK_itemTable) REFERENCES itemTable(id)
+);
