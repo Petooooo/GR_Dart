62e53b8c6a4a2c61fa4eca119612e1db762bce5e
Peto <peto@khu.ac.kr>
2023-11-07 19:15:52 +0900

    Modified .gitignore & Add env.json

---
+++ Server/RESTAPI/restapi.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const mysql = require("mysql");
+
+const envFile = fs.readFileSync("./env.json", "utf8");
+const envData = JSON.parse(envFile);
+
+const conn = {
+    host: envData.host,
+    port: envData.port,
+    user: envData.user,
+    password: envData.pw,
+    database: envData.db,
+};
+
+var connection = mysql.createConnection(conn); // DB 커넥션 생성
+connection.connect(); // DB 접속
+
+// 쿼리문
+
+connection.end(); // DB 접속 종료
