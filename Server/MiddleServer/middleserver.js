8d8e9f0d7253b36a129000b4545833b3f701c145
Peto <peto@khu.ac.kr>
2023-11-08 21:11:44 +0900

    Initialize middleserver.js

---
+++ Server/MiddleServer/middleserver.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const portnum = 8081;
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+    response.send(`<h1>Main Page</h1>`);
+});
+
+// 404 Error Handler
+app.use(function (req, res, next) {
+    res.status(404).send("404 Error: Page Not Found");
+});
+
+// Start Server on Port ${portnum}
+app.listen(portnum, () => {
+    console.log("Listening on " + portnum);
+});
