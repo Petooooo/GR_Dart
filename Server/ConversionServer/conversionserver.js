cc088e41cd7a50156bfbb08d0fa3377a8f8379eb
Peto <peto@khu.ac.kr>
2023-11-27 05:16:05 +0900

    Initialize Conversion Server

--- Server/ConversionServer/conversionserver.js
+++ Server/ConversionServer/conversionserver.js
@@ -1,14 +1,65 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+
+const envFile = fs.readFileSync("./env.json", "utf8");
+const envData = JSON.parse(envFile);
 
 const app = express();
-const portnum = 8081;
+const portnum = 8082;
 
 // Body Parser Middleware
 app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
     response.send(`<h1>Main Page</h1>`);
+});
+
+// [API]     Review Content API
+// [GET]     http://facadeserver:8080/review/content?id=${id}&page=${page}&size=${size}
+// [Example] http://localhost:8080/review/content?id=13078030340&page=1&size=5
+// [cUrl]    curl -X GET "http://localhost:8080/review/content?id=13078030340&page=1&size=5"
+app.get("/review/content", (req, res) => {
+    request.get(
+        {
+            url:
+                "http://" +
+                envData.dbserver_host +
+                ":" +
+                envData.dbserver_port +
+                "/review/content?id=" +
+                req.query.id +
+                "&page=" +
+                req.query.page +
+                "&size=" +
+                req.query.size,
+        },
+        function (error, response, body) {
+            res.send(body);
+        }
+    );
+});
+
+// [API]     Review Delete API
+// [DELETE]  http://facadeserver:8080/review/delete?id=${review_id}
+// [Example] http://localhost:8080/review/delete?id=1
+// [cUrl]    curl -X DELETE "http://localhost:8080/review/delete?id=1"
+app.delete("/review/delete", (req, res) => {
+    request.delete(
+        {
+            url:
+                "http://" +
+                envData.dbserver_host +
+                ":" +
+                envData.dbserver_port +
+                "/review/delete?id=" +
+                req.query.id,
+        },
+        function (error, response, body) {
+            res.send(body);
+        }
+    );
 });
 
 // 404 Error Handler
