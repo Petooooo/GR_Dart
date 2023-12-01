2a64e4e770a798d685f52a7c849fffb4c19b76ac
Peto <peto@khu.ac.kr>
2023-12-01 13:14:18 +0900

    Change logo file & Add Item Lists

--- Client/green_reviewer/lib/main.dart
+++ Client/green_reviewer/lib/main.dart
@@ -10,7 +10,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: SearchBarWidget(),
+        body: Column(
+          children: [
+            SearchBarWidget(),
+            Expanded(
+              child: ProductList(),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -42,56 +49,76 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
+      child: Row(
         children: [
-          Row(
-            children: [
-              GestureDetector(
-                onTap: _onLogoClick, // 로고 이미지 클릭 이벤트 함수 지정
-                child: Image.asset('../assets/logo.jpg', width: 128, height: 64),
-              ),
-              SizedBox(width: 8),
-              Expanded(
-                child: TextField(
-                  controller: _searchController,
-                  focusNode: _focusNode,
-                  onSubmitted: (value) {
-                    _updateSearchResult(value);
+          SizedBox(width: 32),
+          GestureDetector(
+            onTap: _onLogoClick,
+            child: Image.asset('assets/logo.png', width: 128, height: 64),
+          ),
+          SizedBox(width: 32),
+          Expanded(
+            child: TextField(
+              controller: _searchController,
+              focusNode: _focusNode,
+              onSubmitted: (value) {
+                _updateSearchResult(value);
+              },
+              decoration: InputDecoration(
+                labelText: 'Search',
+                labelStyle: TextStyle(color: _isFocused ? Color(0xff19583E) : Color(0xff656565)),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search, color: Color(0xff19583E)),
+                  onPressed: () {
+                    _updateSearchResult(_searchController.text);
                   },
-                  decoration: InputDecoration(
-                    labelText: 'Search',
-                    labelStyle: TextStyle(color: _isFocused ? Color(0xff19583E) : Color(0xff656565)),
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.search, color: Color(0xff19583E)),
-                      onPressed: () {
-                        _updateSearchResult(_searchController.text);
-                      },
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xff19583E), width: 1.5),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xff19583E), width: 2.0),
-                    ),
-                  ),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff19583E), width: 1.5),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff19583E), width: 2.0),
                 ),
               ),
-            ],
+            ),
           ),
-          SizedBox(height: 16.0),
-          Text(_searchResult),
+          SizedBox(width: 32),
         ],
       ),
     );
   }
 
-  void _onLogoClick() { // 로고 이미지 클릭 이벤트 처리
+  void _onLogoClick() {
     print('로고 이미지 클릭!');
   }
 
-  void _updateSearchResult(String value) { // 검색창에서 엔터를 누르거나 검색 버튼을 눌렀을 때 이벤트 처리
+  void _updateSearchResult(String value) {
     setState(() {
       _searchResult = '검색 결과: $value';
     });
+  }
+}
+
+class ProductList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Replace this with your actual list of products
+    List<String> products = ['Product 1', 'Product 2', 'Product 3'];
+
+    return ListView.builder(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0), // Add margins here
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.black)), // Underline only
+            ),
+            padding: EdgeInsets.all(32.0),
+            child: Text(products[index]),
+          ),
+        );
+      },
+    );
   }
 }
